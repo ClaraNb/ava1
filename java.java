dc375a40e861b89eaa830da426ad3e4162d0b4b0
Igor Yuki <igor@agenciavinci.com.br>
2022-09-15 13:14:43 -0300

    commit me

---
+++ java.java
@@ -0,0 +1,46 @@
+public class Main {
+    package src.pacote5;
+
+    public class Main {
+
+    public class Pessoa {
+        private String piloto;
+        private String cpf;
+
+        public String getpiloto() {
+
+            public void setpiloto(String piloto) {
+                public String getcpf() {
+                    public void setcpf(String cpf)
+                }
+            }
+        }
+    }
+}
+
+package src.pacote5;
+public class piloto extends Pessoa {
+    private int cpf;
+    private String aeronave;
+
+    public String getaeronave;
+    public void setaeronave(int aeronave) {
+        
+    }
+}
+
+package src.pacote5.subpacote
+
+import src.pacote5.piloto;
+
+public class voo
+
+public static void main(String [] args) {
+    piloto mot = new piloto();
+
+    mot.setnome("zézinho");
+    mot.setcpf("666");
+
+    system.out.printf("cpf so piloto %s: %s", mot.getnome(), mot.getcpf());
+
+}
